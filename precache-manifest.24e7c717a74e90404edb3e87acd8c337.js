ea902a587240b0524763e746b75d73644a47107c
Aviv Sabag <avivsabag2252@gmail.com>
2022-07-03 15:53:01 +0300

    Updates

---
+++ precache-manifest.24e7c717a74e90404edb3e87acd8c337.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bbd160294a68dde1199",
+    "url": "/MonsterFriends/static/css/main.2cae451d.chunk.css"
+  },
+  {
+    "revision": "0bbd160294a68dde1199",
+    "url": "/MonsterFriends/static/js/main.0bbd1602.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/MonsterFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "6145d626f79ca0ac0d61",
+    "url": "/MonsterFriends/static/css/2.08d52b72.chunk.css"
+  },
+  {
+    "revision": "6145d626f79ca0ac0d61",
+    "url": "/MonsterFriends/static/js/2.6145d626.chunk.js"
+  },
+  {
+    "revision": "a9dacb1d8465588c5188",
+    "url": "/MonsterFriends/static/js/runtime~main.a9dacb1d.js"
+  },
+  {
+    "revision": "99ad3295ae3e2b3674ba69d159bd29ba",
+    "url": "/MonsterFriends/static/media/AneromeStamped-EaLRj.99ad3295.woff"
+  },
+  {
+    "revision": "0ce132406e529dacfaa49b448aeebbc6",
+    "url": "/MonsterFriends/index.html"
+  }
+];
